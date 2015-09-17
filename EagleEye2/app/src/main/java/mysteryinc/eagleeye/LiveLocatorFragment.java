201e4b2661dfecd07b3716909a208bf6ef50b69f
unknown <bassett.random@gmail.com>
2015-09-16 23:48:43 -0400

    Initial commit; App skeleton/navigation framework complete.

---
+++ EagleEye2/app/src/main/java/mysteryinc/eagleeye/LiveLocatorFragment.java
@@ -0,0 +1,40 @@
+package mysteryinc.eagleeye;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LiveLocatorFragment extends Fragment {
+
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "test";
+    private static final int SECTION_NUMBER = 0;
+    private static LiveLocatorFragment _instance;
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static LiveLocatorFragment get_instance() {
+        if (_instance == null) {
+            _instance = new LiveLocatorFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, SECTION_NUMBER);
+            _instance.setArguments(args);
+            _instance.setHasOptionsMenu(true);
+        }
+        return _instance;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.live_locator_fragment, container, false);
+        return rootView;
+    }
+}
