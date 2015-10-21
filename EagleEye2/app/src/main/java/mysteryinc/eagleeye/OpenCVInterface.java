5d2aa41306e18b40f3b8f1ecabe93b752130f10b
jrach190 <jrach190@gmail.com>
2015-10-21 14:24:18 -0400

    commit

--- EagleEye2/app/src/main/java/mysteryinc/eagleeye/OpenCVInterface.java
+++ EagleEye2/app/src/main/java/mysteryinc/eagleeye/OpenCVInterface.java
@@ -1,11 +1,32 @@
 /**
 * This Class file contains the methods necessary to implement OpenCV using the Camera
 */
-public class OpenCVInterface {
+public class OpenCVInterface extends Activity implements CvCameraViewListener {
 
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case LoaderCallbackInterface.SUCCESS:
+                    {
+                        Log.i(TAG, "OpenCV loaded successfully");
+                        mOpenCvCameraView.enableView();
+                    } break;
+                    default:
+                    {
+                        super.onManagerConnected(status);
+                    } break;
+                }
+            }
+        };
 
+        @Override
+        public void onResume()
+        {
+            super.onResume();
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+        }
 
-
-
-    
+        ...
+    }
 }
