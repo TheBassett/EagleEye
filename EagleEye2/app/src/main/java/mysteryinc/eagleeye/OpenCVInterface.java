8e23e4cd6703ee36b83e3846c5392bfeb51fbb20
Jess Updegrove <jessupde@gmail.com>
2015-10-23 11:31:48 -0400

    I really hope this doesn't break everything but I got the OpenCV class to work and added opencv to the project (I think)...

--- EagleEye2/app/src/main/java/mysteryinc/eagleeye/OpenCVInterface.java
+++ EagleEye2/app/src/main/java/mysteryinc/eagleeye/OpenCVInterface.java
@@ -1,7 +1,26 @@
+package mysteryinc.eagleeye;
+
+import android.app.Activity;
+import org.opencv.android.*;
+import org.opencv.android.CameraBridgeViewBase.*;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.*;
+
+import android.util.Log;
 /**
 * This Class file contains the methods necessary to implement OpenCV using the Camera
 */
-public class OpenCVInterface extends Activity implements CvCameraViewListener {
+public class OpenCVInterface extends Activity implements CvCameraViewListener2 {
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private CameraBridgeViewBase.CvCameraViewFrame inputFrame;
+    private static final String TAG = "OCVSample::Activity";
+    private Mat mRgba;
+    private Mat mGray;
+
+    public OpenCVInterface() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -20,13 +39,29 @@ public class OpenCVInterface extends Activity implements CvCameraViewListener {
             }
         };
 
-        @Override
-        public void onResume()
-        {
-            super.onResume();
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
-        }
 
-        ...
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        Imgproc.cvtColor(mRgba, mGray, Imgproc.COLOR_BGRA2GRAY);
+        return mGray;
     }
 }
