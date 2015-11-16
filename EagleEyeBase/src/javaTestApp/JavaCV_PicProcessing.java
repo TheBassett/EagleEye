8981f2b31846775c839f23962d9bab98c79b7b74
Joel Vande Polder <vandepoj@my.erau.edu>
2015-11-16 16:38:46 -0500

    match method added

--- EagleEyeBase/src/javaTestApp/JavaCV_PicProcessing.java
+++ EagleEyeBase/src/javaTestApp/JavaCV_PicProcessing.java
@@ -4,6 +4,7 @@ import static org.bytedeco.javacpp.opencv_face.createLBPHFaceRecognizer;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacpp.opencv_core.RectVector;
 import org.bytedeco.javacpp.opencv_core.Size;
 import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
@@ -13,17 +14,18 @@ import org.bytedeco.javacpp.opencv_imgproc;
 public class JavaCV_PicProcessing {
 	
 	String cascade = new String("/Users/joelvandepolder/Desktop/cascade.xml"); 
+	FaceRecognizer bldgRec = createLBPHFaceRecognizer();
 	private CascadeClassifier faceCascade;
 	double scale = 1.1;
 	int minNeighbors = 1;
 	double minSizeRatio = .1f;
     double maxSizeRatio = .3f;
+    RectVector found;
     
-    public void findBLDG(String ImgPath, RectVector res ){
-		Mat img = imread(ImgPath);
+    public void findBLDG(Mat img, RectVector res ){
 		Mat tmp = new Mat();
 		faceCascade = new CascadeClassifier();
-		faceCascade.load("/Users/joelvandepolder/Desktop/cascade.xml");
+		faceCascade.load(cascade);
 		int width = img.size().width();
 		int height = img.size().height();
 		
@@ -35,19 +37,30 @@ public class JavaCV_PicProcessing {
 		faceCascade.detectMultiScale(tmp, res, scale, minNeighbors, 0, minScaleSize, maxScaleSize);	
 	}
     
-    public String BLDGRecognizer(String path){
+    public int BLDGRecognizer(String path){
     	FaceRecognizer bldgRec = createLBPHFaceRecognizer();
 		bldgRec.load("test.xml");
 		
 		Mat testImg = imread(path, CV_LOAD_IMAGE_GRAYSCALE);
 		
-		int res = bldgRec.predict(testImg);
-		
-		//This will need to change based on labels we gather 
-		switch (res) {
-			case 1: return "COA";
-			case 2: return "COAS";
-			default: return "not an erau bldg";
+		return bldgRec.predict(testImg);
 		}
-    }
+   
+    
+    public String match(String path){
+    	Mat img = imread(path);
+    	findBLDG(img, found);
+    	
+    	for (long n = 0; n < found.size(); n++ ){
+    		Rect tmpRect = found.get(n);
+			Mat tmpMat = new Mat(img, tmpRect);
+			
+			int id = BLDGRecognizer(path);
+			 System.out.print(id);
+    	}
+    	
+    	return null;
+    
+    
 }
+ }
